ffbf8ac0fd7e5961e07a87f0399a91b8852f2fc7
pwolfe8 <philip.wolfe@gtri.gatech.edu>
2022-06-02 18:27:53 -0400

    fixed it

--- package/skaimsginterface/skaimessages/SkaiMessages.py
+++ package/skaimsginterface/skaimessages/SkaiMessages.py
@@ -137,12 +137,10 @@ class SkaiMsg(ABC):
     @staticmethod
     def convert_camera_id_to_mac_addr_string(int_or_list):
         if isinstance(int_or_list, list):
-            return [SkaiMsg.convert_mac_addr_to_camera_identifier_number(num) for num in int_or_list]
+            return [SkaiMsg.convert_camera_id_to_mac_addr_string(num) for num in int_or_list]
         elif isinstance(int_or_list, int):
-            print('integer!')
-            int_or_list = f'{int_or_list:0>12X}'
-            # return ':'.join([int_or_list[i:i+2] for i in range(0, len(int_or_list),2)])
-            return int_or_list
+            hexstr = f'{int_or_list:0>12X}'
+            return ':'.join([hexstr[i:i+2] for i in range(0, len(hexstr),2)])
         else:
             print('not integer!')
             print(f'unsupported type for conversion: {type(int_or_list)}')
