cfa781693ffe26379ab68df6b39a84571f882810
skaiaurora2 <skaiaurora2@noemail.noemail>
2022-06-16 13:22:51 -0400

    formatting for easy error viewing

--- push_all.sh
+++ push_all.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+echo ""
+echo "================================"
 echo "trying to push to skai remote..."
+echo "================================"
 git push skai_remote
+echo ""
+echo "================================"
 echo "trying to push to gtri remote..."
+echo "================================"
 git push gtri_remote
+echo ""
