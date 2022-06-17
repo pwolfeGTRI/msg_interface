f35e0c1a6d056c5c70f6773319718a3f02b0a2da
skaiaurora2 <skaiaurora2@noemail.noemail>
2022-06-17 16:59:25 -0400

    example chagne

--- push_all_remotes.sh
+++ push_all_remotes.sh
@@ -11,7 +11,7 @@ echo "================================"
 git push skai_remote
 echo ""
 echo "================================"
-echo "trying to push to gtri remote..."
+echo "force pushing to gtri remote..."
 echo "================================"
 git push gtri_remote --force
 echo ""
